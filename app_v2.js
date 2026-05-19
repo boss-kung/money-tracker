4d867a354cf70b158843cba7d380419404e718a7
lgrth <thanaphornbosskung@gmail.com>
2026-05-20 02:03:07 +0700

    Fix finance explain panel expansion

--- app_v2.js
+++ app_v2.js
@@ -877,7 +877,7 @@ window.__mountUpcomingBillsFeature = function() {
    Vanilla JS, no build tools, works on file:// and GitHub Pages
    ============================================================ */
 
-const APP_VERSION = '2026.05.20-r21'
+const APP_VERSION = '2026.05.20-r22'
 window.MT_APP_VERSION = APP_VERSION
 
 /* ============================================================
--- service-worker_v2.js
+++ service-worker_v2.js
@@ -1,4 +1,4 @@
-const APP_VERSION = '2026.05.20-r21'
+const APP_VERSION = '2026.05.20-r22'
 const CACHE_PREFIX = 'money-tracker-v2'
 const CACHE_NAME = `${CACHE_PREFIX}-${APP_VERSION}`
 
